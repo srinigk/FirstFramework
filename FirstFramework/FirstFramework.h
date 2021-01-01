418db2cbec6925974398c98362548abf4d8234f0
G Srinivasa <srinivasa.g@connectm.com>
2021-01-01 10:04:34 +0530

    Initial Commit

---
+++ FirstFramework/FirstFramework.h
@@ -0,0 +1,19 @@
+//
+//  FirstFramework.h
+//  FirstFramework
+//
+//  Created by G Srinivasa on 01/01/21.
+//  Copyright © 2021 G Srinivasa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FirstFramework.
+FOUNDATION_EXPORT double FirstFrameworkVersionNumber;
+
+//! Project version string for FirstFramework.
+FOUNDATION_EXPORT const unsigned char FirstFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FirstFramework/PublicHeader.h>
+
+
